0ae8d8c3a3074f367a3463247c5da60c33576155
Jürgen Jung <juergen.jung@ailey.de>
2019-06-27 19:14:07 +0200

    Güter bearbeiten in Vorbereitung

--- Bauwagen/Frm_Admin.Designer.cs
+++ Bauwagen/Frm_Admin.Designer.cs
@@ -31,14 +31,14 @@
             this.CmD_CreateTables = new System.Windows.Forms.Button();
             this.CmD_CreateUser = new System.Windows.Forms.Button();
             this.CmD_Geldaufladen = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.CmD_GüterBearbeiten = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // CmD_CreateTables
             // 
             this.CmD_CreateTables.Location = new System.Drawing.Point(12, 197);
             this.CmD_CreateTables.Name = "CmD_CreateTables";
-            this.CmD_CreateTables.Size = new System.Drawing.Size(111, 25);
+            this.CmD_CreateTables.Size = new System.Drawing.Size(130, 25);
             this.CmD_CreateTables.TabIndex = 0;
             this.CmD_CreateTables.Text = "Tabellen erstellen";
             this.CmD_CreateTables.UseVisualStyleBackColor = true;
@@ -49,7 +49,7 @@
             this.CmD_CreateUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.CmD_CreateUser.Location = new System.Drawing.Point(12, 12);
             this.CmD_CreateUser.Name = "CmD_CreateUser";
-            this.CmD_CreateUser.Size = new System.Drawing.Size(111, 60);
+            this.CmD_CreateUser.Size = new System.Drawing.Size(130, 60);
             this.CmD_CreateUser.TabIndex = 1;
             this.CmD_CreateUser.Text = "User anlegen";
             this.CmD_CreateUser.UseVisualStyleBackColor = true;
@@ -58,31 +58,32 @@
             // CmD_Geldaufladen
             // 
             this.CmD_Geldaufladen.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.CmD_Geldaufladen.Location = new System.Drawing.Point(12, 101);
+            this.CmD_Geldaufladen.Location = new System.Drawing.Point(341, 12);
             this.CmD_Geldaufladen.Name = "CmD_Geldaufladen";
-            this.CmD_Geldaufladen.Size = new System.Drawing.Size(111, 60);
+            this.CmD_Geldaufladen.Size = new System.Drawing.Size(130, 60);
             this.CmD_Geldaufladen.TabIndex = 1;
             this.CmD_Geldaufladen.Text = "Geld aufladen";
             this.CmD_Geldaufladen.UseVisualStyleBackColor = true;
             this.CmD_Geldaufladen.Click += new System.EventHandler(this.CmD_Geldaufladen_Click);
             // 
-            // button1
+            // CmD_GüterBearbeiten
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(155, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(111, 60);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Getränke anlegen";
-            this.button1.UseVisualStyleBackColor = true;
+            this.CmD_GüterBearbeiten.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CmD_GüterBearbeiten.Location = new System.Drawing.Point(175, 12);
+            this.CmD_GüterBearbeiten.Name = "CmD_GüterBearbeiten";
+            this.CmD_GüterBearbeiten.Size = new System.Drawing.Size(130, 60);
+            this.CmD_GüterBearbeiten.TabIndex = 1;
+            this.CmD_GüterBearbeiten.Text = "Getränke bearbeiten";
+            this.CmD_GüterBearbeiten.UseVisualStyleBackColor = true;
+            this.CmD_GüterBearbeiten.Click += new System.EventHandler(this.CmD_GüterBearbeiten_Click);
             // 
             // Frm_Admin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(417, 234);
+            this.ClientSize = new System.Drawing.Size(881, 234);
             this.Controls.Add(this.CmD_Geldaufladen);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.CmD_GüterBearbeiten);
             this.Controls.Add(this.CmD_CreateUser);
             this.Controls.Add(this.CmD_CreateTables);
             this.Name = "Frm_Admin";
@@ -97,6 +98,6 @@
         private System.Windows.Forms.Button CmD_CreateTables;
         private System.Windows.Forms.Button CmD_CreateUser;
         private System.Windows.Forms.Button CmD_Geldaufladen;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button CmD_GüterBearbeiten;
     }
 }
